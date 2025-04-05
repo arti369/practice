fa98d9d71b0af72eb2ad7d9f0d875caab93efdb9
hanminseo <tlfxla45@naver.com>
2025-04-05 23:49:40 +0900

    test

---
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class HelloController {
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트입니다.";
+    }
+}
